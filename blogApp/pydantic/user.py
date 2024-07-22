3b13012a080af8737a6a3bd71e689bfaa18ec884
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-07-22 17:47:44 +0300

    pydantic user/post relationship. UserPost model

--- blogApp/pydantic/user.py
+++ blogApp/pydantic/user.py
@@ -1,6 +1,15 @@
 from pydantic import BaseModel, ConfigDict
+from typing import List, Union
 
 class User(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     
     username:str
+    
+    
+class UserPost(User):
+
+    posts:Union[List['Post'], None] = None
+
+
+from .post import Post
