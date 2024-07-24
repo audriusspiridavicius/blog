c69b8184d413bdac6a0a060d6f5b29c66dc1d5f9
Audrius_spiridavicius <audriusspiridavicius@gmail.com>
2024-07-24 16:10:28 +0300

    get user by id endpoint

--- blogApp/routes/users.py
+++ blogApp/routes/users.py
@@ -1,12 +1,14 @@
-from typing import Annotated
+from typing import Annotated, List
 from fastapi import Depends
 
+from blogApp.database import get_database
 from blogApp.functions.login import login_user
 from fastapi import APIRouter
 
 from blogApp.database.user import User
 from blogApp import pydantic
-
+from blogApp.database.crud import retrieve_from_database
+from sqlalchemy.orm import Session
 
 router = APIRouter()
 
@@ -16,3 +18,8 @@ router = APIRouter()
 def user(user:Annotated[User, Depends(login_user)]):
     
     return user
+
+@router.post("/users/{user_id}", response_model=List[pydantic.User])
+def user(user_id, db:Annotated[Session, Depends(get_database)]):
+    
+    return retrieve_from_database(User,db,id=user_id)
